f5827bbd40754575e3e1387fd619745d97586471
darwin3750 <damedel@mymail.mapua.edu.ph>
2020-11-13 01:10:08 +0800

    finish delete button and toggle read button

--- add_book.js
+++ add_book.js
@@ -1,22 +1,52 @@
 //add "add book" modal form
+
+    //the modal
     const modal = document.createElement("div");
     modal.setAttribute("id", "addBookModal");
     modal.classList.add("modal");
-    modal.textContent = "Modal Test";
 
     const modal_content = document.createElement("div");
-    modal_content
+    modal_content.textContent = "Modal Test";
+    modal_content.classList.add("modal-content");
+    modal.appendChild(modal_content);
 
+    //the form
+    const form = document.createElement("form");
+    for(let property in myLibrary[0]){
+        let label = document.createElement("label");
+        label.setAttribute("for", property);
+        label.textContent = property;
+        let input = document.createElement("input");
+        input.setAttribute("type", "text");
+        input.setAttribute("id", property);
+        form.appendChild(label);
+        form.appendChild(document.createElement("br"));
+        form.appendChild(input);
+        form.appendChild(document.createElement("br"));
+    }
+    //form submit button
+    const form_button = document.createElement("input");
+    form_button.setAttribute("type", "submit");
+    form_button.setAttribute("value", "Submit");
+    form.appendChild(form_button);
+
+    modal_content.appendChild(form);
+
+    //the submit button
+
+    //put in html
     document.querySelector("#main").appendChild(modal);
 
 //add "add book" button
     const button = document.createElement("button");
     button.setAttribute("id", "addBookButton");
     button.textContent = "Add Book";
+    button.classList.add("btn", "btn-block", "btn-primary", "mb-2", "mt-2") //button classes
 
     //add to html
-    document.querySelector("#main").appendChild(button);
+    document.querySelector("#button_section").appendChild(button);
 
+//Listeners
     //button click listener to open modal
     const addBookButton = document.querySelector("#addBookButton");
     addBookButton.addEventListener('click', () => {
--- library.js
+++ library.js
@@ -1,15 +1,15 @@
 let myLibrary = [];
 
-function Book(title, author, num_of_pages, isRead) {
-  // the constructor...
+function Book(id, title, author, num_of_pages, isRead) {
+  this.id=id;
   this.title=title;
   this.author=author;
   this.num_of_pages=num_of_pages;
   this.isRead=isRead;
 }
 
-function addBookToLibrary(title, author, num_of_pages, isRead) {
-  const new_book = new Book(title, author, num_of_pages, isRead);
+function addBookToLibrary(id, title, author, num_of_pages, isRead) {
+  const new_book = new Book(id, title, author, num_of_pages, isRead);
   myLibrary.push(new_book);
 }
 
@@ -18,12 +18,14 @@ function viewBooks(){
 }
 
 //add books
-addBookToLibrary("title", "author", 3, false);
-addBookToLibrary("title", "author", 3, false);
-addBookToLibrary("title", "author", 3, false);
-addBookToLibrary("title", "author", 3, false);
+addBookToLibrary("1", "titleA", "author", 3, false);
+addBookToLibrary("2", "titleB", "author", 3, false);
+addBookToLibrary("3", "titleC", "author", 3, false);
+addBookToLibrary("4", "titleD", "author", 3, false);
 
 //put books in table
+function insertBooksTable(){
+    document.querySelector("#table_section").innerHTML = "";
     let table = document.createElement("table");
     table.classList.add("table"); //table classes
 
@@ -37,15 +39,53 @@ addBookToLibrary("title", "author", 3, false);
     table.appendChild(headingRow);
 
     //add table row content
-    for(let i=0; i<myLibrary.length; i++){
+    for(let i=0; i<myLibrary.length; i++){ //loop through myLibrary
         let tr = document.createElement("tr");
-        for(let property in myLibrary[i]){
+        for(let property in myLibrary[i]){ //loop through columns
             let td = document.createElement("td");
             td.textContent = myLibrary[i][property];
             tr.appendChild(td);
         }
+        //delete button
+        const delete_button = document.createElement("button");
+        let td = document.createElement("td");
+        delete_button.textContent = "Delete";
+        delete_button.classList.add("btn", "btn-danger", "delete_button")
+        td.appendChild(delete_button);
+        tr.appendChild(td);
+        //toggle read button
+        const read_button = document.createElement("button");
+        td = document.createElement("td");
+        read_button.textContent = "Toggle Read";
+        read_button.classList.add("btn", "btn-info", "read_button")
+        td.appendChild(read_button);
+        tr.appendChild(td);
+
         table.appendChild(tr);
     }
 
     //add table to HTML
-    document.querySelector("#main").appendChild(table);
+    document.querySelector("#table_section").appendChild(table);
+    setListeners();
+}
+
+//Listeners
+function setListeners(){
+    let delete_buttons = document.querySelectorAll(".delete_button");
+    let read_buttons = document.querySelectorAll(".read_button");
+    delete_buttons = Array.from(delete_buttons);
+    read_buttons = Array.from(read_buttons);
+    for(let i=0; i<delete_buttons.length; i++){
+        delete_buttons[i].addEventListener('click', (e)=>{
+            myLibrary.splice(myLibrary.indexOf(myLibrary.find(book => book.id === e.target.parentNode.parentNode.firstChild.textContent)), 1)
+            insertBooksTable();
+        });
+        read_buttons[i].addEventListener('click', (e)=>{
+            myLibrary.splice(myLibrary.indexOf(myLibrary.find(book => book.id === (obj=e.target.parentNode.parentNode).firstChild.textContent)), 1, 
+                    new Book(obj.childNodes[0].textContent, obj.childNodes[1].textContent, obj.childNodes[2].textContent, obj.childNodes[3].textContent, (obj.childNodes[4].textContent=="false" ? true : false)));
+            insertBooksTable();
+        });
+    }
+}
+
+insertBooksTable();
