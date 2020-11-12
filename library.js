67beebc0284569d714df1e288e5b8d5aed79cb09
darwin3750 <damedel@mymail.mapua.edu.ph>
2020-11-12 16:05:59 +0800

    display library books in HTML table

--- library.js
+++ library.js
@@ -1,21 +1,49 @@
-let myLibrary = ["test1", "test2"];
+let myLibrary = [];
 
-function Book(title, author) {
+function Book(title, author, num_of_pages, isRead) {
   // the constructor...
   this.title=title;
   this.author=author;
+  this.num_of_pages=num_of_pages;
+  this.isRead=isRead;
 }
 
-function addBookToLibrary(book) {
-  // do stuff here
-  myLibrary.push(book);
+function addBookToLibrary(title, author, num_of_pages, isRead) {
+  const new_book = new Book(title, author, num_of_pages, isRead);
+  myLibrary.push(new_book);
 }
 
 function viewBooks(){
     myLibrary.forEach((val)=>{console.log(val)})
 }
 
+//test book
+addBookToLibrary("title", "author", 3, false);
 
-//write to HTML
+//put books to table
+    let table = document.createElement("table");
+    table.style["border"] = "solid 1px black";
 
-viewBooks();
+    //add table heading
+    let headingRow = document.createElement("tr");
+    for(let property in myLibrary[0]){
+        let th = document.createElement("th");
+        th.textContent = property;
+        headingRow.appendChild(th);
+    }
+    table.appendChild(headingRow);
+
+    //add table row content
+    for(let i=0; i<myLibrary.length; i++){
+        let tr = document.createElement("tr");
+        for(let property in myLibrary[i]){
+            let td = document.createElement("td");
+            td.style["border"] = "solid 1px black";
+            td.textContent = myLibrary[i][property];
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+
+    //add table to HTML
+    document.querySelector("#main").appendChild(table);
